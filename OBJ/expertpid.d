8cda4c58a77d8480935d7013d95ef078ea478c11
huangms1 <871033225@qq.com>
2025-09-25 11:54:27 +0800

    清理中间文件并应用 .gitignore

--- OBJ/expertpid.d
+++
@@ -1,6 +0,0 @@
-..\obj\expertpid.o: ..\HARDWARE\PID\expertpid.c
-..\obj\expertpid.o: ..\HARDWARE\PID\expertpid.h
-..\obj\expertpid.o: ..\HARDWARE\PID\fuzzypid.h
-..\obj\expertpid.o: D:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
-..\obj\expertpid.o: D:\Keil_v5\ARM\ARMCC\Bin\..\include\stdbool.h
-..\obj\expertpid.o: D:\Keil_v5\ARM\ARMCC\Bin\..\include\math.h
